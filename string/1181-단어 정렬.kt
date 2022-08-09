82041200ba4d07ede5a8df1bef8fe22f9e508635
murjune <sis9221302@gmail.com>
2022-08-09 20:15:17 +0900

    [sol] 단어 정렬

---
+++ string/1181-단어 정렬.kt
@@ -0,0 +1,23 @@
+package calculator
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+fun main() {
+    val cnt = br.readLine().toInt()
+    val arr = ArrayList<String>()
+    repeat(cnt) {
+        arr.add(br.readLine())
+    }
+    arr.distinct().sortedWith(
+        compareBy<String> { it.length }.thenBy { it }
+    ).forEach { bw.write("$it\n") }
+
+    bw.flush()
+    bw.close()
+    br.close()
+}
