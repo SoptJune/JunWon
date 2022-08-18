abf72aa3c279b3ea8147c9aaecea048c3798de3c
JUNWON LEE <87055456+murjune@users.noreply.github.com>
2022-08-18 19:44:49 +0900

    [fix] 컵홀더  python풀이 재업로드

--- string/2810-컵홀더/1.py
+++ string/2810-컵홀더/1.py
@@ -4,12 +4,16 @@ input = lambda: sys.stdin.readline().rstrip()
 print = lambda x: sys.stdout.write(str(x) + "\n")
 
 n = int(input())
-trees = list(map(int, input().split()))
-trees.sort(reverse=True) # 내림차순으로 정렬
-ans = -1 # 초깃값을 그냥 -1로 세팅했습니다 제 취향..
-
-for idx, tree in enumerate(trees):
-    tmp = tree + idx + 1 # 해당 나무 자라는 시간(tree) + 이전 나무들 심는데 걸린 시간(idx) + 나무 심는데 걸리는 시간(1)
-    ans = max(tmp, ans) 
-print(ans + 1) # 이장님은 나무 다 심은 뒤 하루 뒤 오시니까 +1 ㅎ ㅎ ㅎ
+cnt = 1  # 처음 좌석 왼쪽에 컵홀더가 1개 있으니 1부터 시작
+# 컵 홀더의 수는 좌석의 오른쪽만 세도록하자
+seats = input()
+idx = 0
+while (idx < n):
+    if (seats[idx] == 'S'):
+        cnt += 1
+        idx += 1
+    else:
+        cnt += 1
+        idx += 2
+print(n if n < cnt else cnt)
 
