e5817d3127fcef859cfdb05ea61cc4b7a51e07ad
murjune <sis9221302@gmail.com>
2022-08-18 20:41:32 +0900

    [sol] 듣보잡 kotlin풀이

---
+++ set/1746-듣보잡/1.kt
@@ -0,0 +1,25 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+fun main() {
+    val (n, m) = br.readLine().split(" ")
+    val setA = HashSet<String>()
+    val setB = HashSet<String>()
+    repeat(n.toInt()) {
+        setA.add(br.readLine())
+    }
+    repeat(m.toInt()) {
+        setB.add(br.readLine())
+    }
+    val setC = setA.intersect(setB).sorted()
+    bw.write("${setC.size}\n")
+    setC.forEach { bw.write("$it\n") }
+    bw.flush()
+    bw.close()
+    br.close()
+}
+
