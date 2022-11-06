6cd69272c6135cc01873cb7f3237657f93da53f2
JUNWON LEE <87055456+murjune@users.noreply.github.com>
2022-11-07 07:37:35 +0900

    [fix] 베스트 앨범 풀이 수정

--- 프로그래머스/sort/베스트앨범.kt
+++ 프로그래머스/sort/베스트앨범.kt
@@ -11,16 +11,12 @@ class Solution {
             stepOneMusicList.map { pair -> pair.second }.reduce { total, playNum -> total + playNum }
         } // 2. 재생 수 많은 수 (노래) - 내림차순
         .map { stepOneMusicList: List<Pair<Int, Int>> ->
-            val stepTwoMusicList = stepOneMusicList.sortedByDescending { musicPair ->
-                musicPair.second
-            }.take(2)
-            // 3. 재생수 같으면 고유 번호 작은 수 (고유 번호) - 오름차순
-            if (stepTwoMusicList.size == 1) return@map stepTwoMusicList.map { pair -> pair.first }
-            if (stepTwoMusicList[0].second != stepTwoMusicList[1].second) return@map stepTwoMusicList.map { pair -> pair.first }
-            if (stepTwoMusicList[0].first <= stepTwoMusicList[1].first) return@map stepTwoMusicList.map { pair -> pair.first }
-
-            Collections.swap(stepTwoMusicList, 0, 1)
-            stepTwoMusicList.map { pair -> pair.first }
-        }
-        .flatten().toIntArray()
-}}
+            stepOneMusicList.sortedWith(
+                compareBy(
+                    {
+                        -it.second
+                    }, { it.first }
+                )).take(2).map { it.first }
+        }.flatten().toIntArray()
+    }
+}
