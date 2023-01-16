b1471c74caa5c1a89604b00db775dffe166ed32d
murjune <sis9221302@gmail.com>
2023-01-16 16:30:17 +0900

    크루스칼 알고리즘 기초 세팅

---
+++ 기초 세팅/kruskal.kt
@@ -0,0 +1,24 @@
+val parents = IntArray(1001) { -1 } // Node의 개수로 초기화
+
+fun findParent(curNode: Int): Int {
+    if (parents[curNode] <= -1) return curNode
+
+    parents[curNode] = findParent(parents[curNode])
+    return parents[curNode]
+}
+
+fun union(x: Int, y: Int): Boolean {
+    val parentX = findParent(x)
+    val parentY = findParent(y)
+
+    if (parentX == parentY) return false
+
+    if (parents[parentX] >= parents[parentY]) {
+        parents[parentY] += parents[parentX]
+        parents[parentX] = parentY
+    } else {
+        parents[parentX] += parents[parentY]
+        parents[parentY] = parentX
+    }
+    return true
+}
