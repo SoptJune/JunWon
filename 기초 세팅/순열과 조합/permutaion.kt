785f5e091a1dc91036f5bb11fbfb40e51baf6f2c
murjune <sis9221302@gmail.com>
2023-01-19 06:09:58 +0900

    fix permutaion function

--- 기초 세팅/순열과 조합/permutaion.kt
+++ 기초 세팅/순열과 조합/permutaion.kt
@@ -6,7 +6,7 @@ fun main() {
     println(permutation(listOf(1,2,3,4,5), target = 4))
     // 출력값 : [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 4, 3], ...
     println(permutation(listOf(1,2,3,4,5), target = 6))
-    // 출력값 : IndexOutOfBoundsException
+    // 출력값 : []
 }
 
 fun <T> permutation(
@@ -15,7 +15,6 @@ fun <T> permutation(
     sub: List<T> = el,
     target: Int = el.size
 ): List<List<T>> {
-    if (target > el.size) throw IndexOutOfBoundsException("target이 el의 size보다 큽니다!!")
     return if (target == 0) listOf(fin)
     else sub.flatMap { permutation(el, fin + it, sub - it, target - 1) }
 }
