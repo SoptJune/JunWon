3103c14ab6f722faa1ab531b26c2dec9f133354d
murjune <sis9221302@gmail.com>
2023-01-18 16:52:02 +0900

    add BigInterger factorial function

--- 기초 세팅/순열과 조합/factorial.kt
+++ 기초 세팅/순열과 조합/factorial.kt
@@ -2,7 +2,14 @@ fun main() {
     println(factorial(5)) // 120
 }
 
-tailrec fun factorial(n: Int, result: Int): Int {
+/** Int */
+tailrec fun factorial(n: Int, result: Int = 1): Int {
     if (n == 1) return result
     return factorial(n - 1, result * n)
+}
+
+/** BigInteger */
+tailrec fun factorial(n: Int, ans: BigInteger = BigInteger("1")): BigInteger {
+    if (n == 1) return ans
+    return factorial(n - 1, ans.multiply(BigInteger("$n")))
 }
