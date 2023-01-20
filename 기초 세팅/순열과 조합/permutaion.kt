686cec943e528bdd71f73d95b7b9def217f4ef07
murjune <sis9221302@gmail.com>
2023-01-20 18:48:21 +0900

    permutation Test

--- 기초 세팅/순열과 조합/permutaion.kt
+++ 기초 세팅/순열과 조합/permutaion.kt
@@ -25,3 +25,83 @@ fun <T> List<T>.permutation(r: Int = this.size, cur: List<T> = emptyList()): Lis
 }
 
 // 얘도 나중에 인덱싱 방식으로 바꿔야할듯.. 공간복잡도, 시간복잡도 너무 많이 사용
+
+// Int List
+private fun List<Int>.permutation(
+    r: Int = this.size,
+    curArr: MutableList<Int> = MutableList(r) { 0 },
+    visited: Int = 0
+) {
+    if (r == 0) {
+        println(curArr)
+        return
+    }
+    forEachIndexed { i, v ->
+        if (visited and (1 shl i) == 0) {
+            curArr[curArr.size - r] = v
+            permutation(r - 1, curArr, visited or (1 shl i))
+        }
+    }
+}
+
+// String List
+private fun List<String>.permutation(
+    r: Int = this.size,
+    curArr: MutableList<String> = MutableList(r) { "" },
+    visited: Int = 0
+) {
+    if (r == 0) {
+        println(curArr)
+        return
+    }
+    forEachIndexed { i, v ->
+        if (visited and (1 shl i) == 0) {
+            curArr[curArr.size - r] = v
+            permutation(r - 1, curArr, visited or (1 shl i))
+        }
+    }
+}
+
+// Int List
+// ex) println(listOf(1, 2, 3).permutationAll())
+// output : [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+private fun List<Int>.permutationAll(
+    r: Int = this.size,
+    curArr: MutableList<Int> = MutableList(r) { 0 },
+    visited: Int = 0,
+    result: MutableList<List<Int>> = mutableListOf()
+): List<List<Int>> {
+    if (r == 0) {
+        result.add(curArr.toList())
+        return emptyList()
+    }
+    forEachIndexed { i, v ->
+        if (visited and (1 shl i) == 0) {
+            curArr[curArr.size - r] = v
+            permutationAll(r - 1, curArr, visited or (1 shl i), result)
+        }
+    }
+    return result
+}
+
+// String List
+// ex) println(listOf("1", "2", "3").permutationAll())
+// output : [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+private fun List<String>.permutationAll(
+    r: Int = this.size,
+    curArr: MutableList<String> = MutableList(r) { "" },
+    visited: Int = 0,
+    result: MutableList<List<String>> = mutableListOf()
+): List<List<String>> {
+    if (r == 0) {
+        result.add(curArr.toList())
+        return emptyList()
+    }
+    forEachIndexed { i, v ->
+        if (visited and (1 shl i) == 0) {
+            curArr[curArr.size - r] = v
+            permutationAll(r - 1, curArr, visited or (1 shl i), result)
+        }
+    }
+    return result
+}
