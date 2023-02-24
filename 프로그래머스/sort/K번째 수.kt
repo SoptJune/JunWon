d54fbca1ea0d3723bb4e5ad93cdb42d7745aa4cc
murjune <sis9221302@gmail.com>
2023-02-24 22:00:57 +0900

    [sort] K번째 수

---
+++ 프로그래머스/sort/K번째 수.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+        return commands.map { command ->
+            val (i, j, k) = command.map{it - 1}
+            array.slice(i .. j).sorted()[k]
+        }.toIntArray()
+    }
+}
