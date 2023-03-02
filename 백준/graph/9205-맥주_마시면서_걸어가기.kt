3afc519169fbfc29481ff1b90be7e13bf171d83b
murjune <sis9221302@gmail.com>
2023-03-03 02:28:10 +0900

    [bfs] 9205-맥주 마시면서 걸어가기

---
+++ 백준/graph/9205-맥주_마시면서_걸어가기.kt
@@ -0,0 +1,44 @@
+import java.util.*
+import kotlin.math.*
+
+private val br = System.`in`.bufferedReader()
+private var n = 0
+private lateinit var visited: MutableMap<List<Int>, Boolean>
+fun main() {
+    visited = mutableMapOf<List<Int>, Boolean>()
+    visited += listOf(1, 2) to true
+    repeat(br.readLine().toInt()) {
+        n = br.readLine().toInt()
+        var (s_x, s_y) = br.readLine().split(" ").map{it.toInt()}
+        visited = mutableMapOf<List<Int>, Boolean>()
+        repeat(n) {
+            visited += br.readLine().split(" ").map{it.toInt()} to false
+        }
+        val graph = visited.keys
+        var (e_x, e_y) = br.readLine().split(" ").map{it.toInt()}
+
+        var ans = "sad"
+
+        val q = LinkedList<Pair<Int, Int>>().apply {
+            add(s_x to s_y)
+        }
+        // println(graph)
+        while(q.isNotEmpty()) {
+            val (x, y) = q.remove()
+            visited[listOf(x, y)] = true
+            if(abs(x - e_x) + abs(y - e_y) <= 1000) {
+                ans = "happy"
+                break
+            }
+
+            graph.forEach {
+                val (n_x, n_y) = it
+                if(!visited[listOf(n_x, n_y)]!! && abs(x - n_x) + abs(y - n_y) <= 1000) {
+                    visited += listOf(n_x, n_y) to true
+                    q.add(n_x to n_y)
+                }
+            }
+        }
+        println(ans)
+    }
+}
