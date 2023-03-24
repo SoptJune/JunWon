bba90954f1fafe0a9047de97a37399686dfbbb1d
murjune <sis9221302@gmail.com>
2023-03-24 17:33:17 +0900

    [binary-search] 2343 기타 레슨

---
+++ 백준/binary-search/2343-기타_레슨.kt
@@ -0,0 +1,35 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (_, m) = br.readLine().split(" ").map{it.toInt()}
+    val videos = br.readLine().split(" ").map{it.toInt()}
+    val cal = {mid: Int ->
+        var cnt = 1
+        var tmp = 0
+        videos.forEach { v ->
+            if(tmp + v > mid)  {
+                tmp = v
+                cnt++
+            }
+            else {
+                tmp += v
+            }
+        }
+        cnt
+    }
+    var s = videos.max()
+    var e = 1e9.toInt()
+    var mid:Int
+    var ans = 1e9.toInt()
+    while(s <= e) {
+        mid = (s + e) / 2
+        if(cal(mid) > m) {
+            s = mid + 1
+        }else {
+            ans = minOf(ans, mid)
+            e = mid - 1
+        }
+    }
+    println(ans)
+}
+// cnt 13
+// 1 2 3 4 5 6 7 8 9
