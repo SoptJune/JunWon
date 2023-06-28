323fb2c94b13c849cebbd012b26374bbd6bc8c12
murjune <sis9221302@gmail.com>
2023-06-28 23:25:10 +0900

    sol : 2294 _동전2

--- src/main/kotlin/baekjun/dp/2294_동전2.kt
+++ src/main/kotlin/baekjun/dp/2294_동전2.kt
@@ -1,15 +1,31 @@
 package baekjun.dp
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
+private val br = System.`in`.bufferedReader()
+private val bw = System.out.bufferedWriter()
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
-
-    val (n, k) = br.readLine().split("")
-    println(n)
-    println(k)
+    val maxValue = 100_001
+    val limit = 10_000
+    // n <= 100, k <= 10,000
+    val (n, k) = br.readLine().split(" ").map { it.toInt() }
+    val values = IntArray(n).apply {
+        repeat(n) {
+            val v = br.readLine().toInt()
+            if(v > limit) return@repeat
+            this[it] = v
+        }
+    }
+    val d = IntArray(10_001) { maxValue }.apply {
+        values.forEach { v ->
+            this[v] = 1
+        }
+    }
+    repeat(k + 1) { i ->
+        values.forEach { v ->
+            val diff = i - v
+            if (diff >= 0) {
+                d[i] = minOf(d[i], d[i - v] + 1)
+            }
+        }
+    }
+    println(if (d[k] == maxValue) -1 else d[k])
 }
